6ed464bb020421b0844634ed33eec046c929e7cf
Ana Santos <anabia200113@gmail.com>
2022-09-03 11:44:17 -0300

    testes

--- my-project-with-vitest/src/App.jsx
+++ my-project-with-vitest/src/App.jsx
@@ -2,13 +2,12 @@ import './App.css'
 import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   //<> react fragment - elemento mãe fica invisível na estrutura e n influencia no CSS
   // em outras palavras, é uma div invisível
   return (
     <>
-      
+      <Counter />
     </>
   )
 }
--- my-project-with-vitest/src/components/Counter.test.jsx
+++ my-project-with-vitest/src/components/Counter.test.jsx
@@ -8,24 +8,40 @@ import Counter from "./Counter";
 * @vitest-environment jsdom
 */ 
 
-// uma função test recebe 2 parâmetros: uma String e uma arrow function
-test('renderizará o valor inicial 0', () => {
-    render(<Counter />) // renderizará um componente
-
-    expect (screen.getByText('0')).toBeInTheDocument()
-    expect(screen.getByTestId('count')).toBeInTheDocument()
-    expect(screen.getByTestId('count')).toHaveTextContent('0')
-
+// meu componente roda antes de cada casinho de test
+describe ('Counter', () => {
+    beforeEach(() => {  // antes de cada caso de test rode isso
+        render (< Counter />) // renderizará um componente
+    })
+    
+    // uma função test recebe 2 parâmetros: uma String e uma arrow function
+    test('renderizará o valor inicial 0', () => {
+    
+        expect (screen.getByText('0')).toBeInTheDocument()
+        expect(screen.getByTestId('count')).toBeInTheDocument()
+        expect(screen.getByTestId('count')).toHaveTextContent('0')
+    
+    })
+    
+    
+    test('deverá acrescentar 1 qnd o botão Add for clicado', () => {
+    
+        expect(screen.getByTestId('add')).toBeInTheDocument()
+    
+        // vem de library
+        fireEvent.click(screen.getByTestId('add'))
+    
+        expect(screen.getByTestId('count')).toHaveTextContent('1')
+    })
+    
+    
+    test('deverá diminuir 1 qnd o botão Substract for clicado', () => {
+    
+        expect(screen.getByTestId('subtract')).toBeInTheDocument()
+    
+        // vem de library
+        fireEvent.click(screen.getByTestId('subtract'))
+    
+        expect(screen.getByTestId('count')).toHaveTextContent('-1')
+    })
 })
-
-test('deverá acrescentar 1 qnd o botão Add for clicado', () => {
-    render(<Counter/>)
-
-    expect(screen.getByTestId('add')).toBeInTheDocument()
-
-    // vem de library
-    fireEvent.click(screen.getByTestId('add'))
-
-    expect(screen.getByTestId('count')).toHaveTextContent('1')
-})
-
